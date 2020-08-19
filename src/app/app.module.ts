c3d53fd636305368e8adad17a53988cc57208b38
nguyenvictor10001@gmail.com <nguyenvictor10001@gmail.com>
2020-08-19 11:15:31 -0400

    nav bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
     BrowserAnimationsModule
   ],
   providers: [],
