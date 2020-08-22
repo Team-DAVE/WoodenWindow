cde878cac49beca14b20e46f50218c3361eb2bb1
Eurus Wagner <euruswagner@gmail.com>
2020-08-21 21:03:33 -0400

    DB connection works and target is ignored

---
+++ backend/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,57 @@
+package com.dao;
+
+import com.model.Users;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Repository
+@Transactional
+public class UserDao {
+    SessionFactory sessionFactory;
+
+  /*@PostConstruct
+  protected void initFlashcard() {
+    Flashcard f = new Flashcard();
+    f.setQuestion("How many beans are required to integrate Spring and Hibernate with Transaction Management");
+    f.setAnswer("3");
+    Session session = sessionFactory.openSession();
+    Transaction tx = session.beginTransaction();
+    session.save(f);
+    tx.commit();
+  }*/
+
+    @Transactional
+    public void addUser(String username, String password) {
+        Users f = new Users();
+        f.setUserName(username);
+        f.setPassword(password);
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(f);
+        tx.commit();
+    }
+
+    @Autowired
+    public UserDao(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, readOnly=true)
+    public List<Users> findAll() {
+        Session session = sessionFactory.getCurrentSession(); // CONTEXTUAL SESSION. IS USED IN TX MANAGEMENT.
+        String sql = "Select u From Users u";
+        Query query = session.createQuery(sql);
+        List<Users> cards = query.list();
+        return cards;
+    }
+}
---
+++ backend/src/main/java/com/driver/Java.java
@@ -0,0 +1,31 @@
+package com.driver;
+
+import com.dao.UserDao;
+import com.model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Java {
+    UserDao flashcardDao;
+
+    @Autowired
+    public Java(UserDao flashcardDao) {
+        this.flashcardDao = flashcardDao;
+    }
+
+    public static void main(String[] args) {
+        AbstractApplicationContext ac = new ClassPathXmlApplicationContext("application-context.xml");
+        Java m = ac.getBean( "java",Java.class);
+        m.flashcardDao.addUser("Testquestion3","answer is 103");
+        List<Users> cards =  m.flashcardDao.findAll();
+        for(Users f : cards) {
+            System.out.println(f);
+        }
+        ac.close();
+    }
+}
