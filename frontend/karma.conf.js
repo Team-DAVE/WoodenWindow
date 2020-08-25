0db9a2e7e7e2ad9a91c3ef202c1fa1f2321e4727
Dakota Wood <cody.wood1943@gmail.com>
2020-08-25 19:04:52 -0400

    moved travis.yml file and dockerfile to inside frontend folder. Also created another travis.yml file inside of backend

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -3,13 +3,6 @@
 
 module.exports = function (config) {
   config.set({
-    browsers: [ 'MyHeadlessChrome' ],
-    customLanchers: {
-      MyHeadlessChrome: {
-        base: 'ChromeHeadless',
-        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox']
-      }
-    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -32,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ['Chrome'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
